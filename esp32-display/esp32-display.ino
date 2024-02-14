2a298417b93a45b3e4d334bd74274b82c738f476
sorcerer_com <sorcerer_com@abv.bg>
2024-02-14 20:43:41 +0200

    ESP32 Display - cyrtext + improvements

--- esp32-display/esp32-display.ino
+++ esp32-display/esp32-display.ino
@@ -4,6 +4,8 @@
 #include <Arduino_GFX_Library.h>
 // https://lvgl.io/tools/imageconverter
 
+#include <map>
+
 #include <WiFi.h>
 #include <WebServer.h>
 #include <HTTPUpdateServer.h>
@@ -12,12 +14,76 @@
 
 #include "FSBrowser.h"
 #include "Font/font.c"
+#include "Font/font_7segm.c"
 
 #define GFX_BL DF_GFX_BL  // default backlight pin, you may replace DF_GFX_BL to actual backlight pin
 #define TFT_BL 2
 
 #define BACKGROUND BLACK
 #define BACKLIGHT 256  // 0 - 256
+
+std::map<char, String> lat_cyr = {
+  { 'a', "а" },
+  { 'A', "А" },
+  { 'b', "б" },
+  { 'B', "Б" },
+  { 'v', "в" },
+  { 'V', "В" },
+  { 'g', "г" },
+  { 'G', "Г" },
+  { 'd', "д" },
+  { 'D', "Д" },
+  { 'e', "е" },
+  { 'E', "Е" },
+  { 'j', "ж" },
+  { 'J', "Ж" },
+  { 'z', "з" },
+  { 'Z', "З" },
+  { 'i', "и" },
+  { 'I', "И" },
+  { '>', "й" },
+  { '<', "Й" },
+  { 'k', "к" },
+  { 'K', "К" },
+  { 'l', "л" },
+  { 'L', "Л" },
+  { 'm', "м" },
+  { 'M', "М" },
+  { 'n', "н" },
+  { 'N', "Н" },
+  { 'o', "о" },
+  { 'O', "О" },
+  { 'p', "п" },
+  { 'P', "П" },
+  { 'r', "р" },
+  { 'R', "Р" },
+  { 's', "с" },
+  { 'S', "С" },
+  { 't', "т" },
+  { 'T', "Т" },
+  { 'u', "у" },
+  { 'U', "У" },
+  { 'f', "ф" },
+  { 'F', "Ф" },
+  { 'h', "х" },
+  { 'H', "Х" },
+  { 'c', "ц" },
+  { 'C', "Ц" },
+  { 'y', "ч" },
+  { 'Y', "Ч" },
+  { '/', "ш" },
+  { '?', "Ш" },
+  { '%', "щ" },
+  { '&', "Щ" },
+  { 'x', "ъ" },
+  { 'X', "Ъ" },
+  { '#', "ь" },
+  { '$', "ѝ" },
+  { 'q', "ю" },
+  { 'Q', "Ю" },
+  { 'w', "я" },
+  { 'W', "Я" },
+};
 
 WebServer httpServer(80);
 HTTPUpdateServer httpUpdater;
@@ -66,18 +132,23 @@ void setup() {
 #endif
 }
 
+// image buffer
+const uint16_t *image = new uint16_t[800 * 600];
+
 void loop() {
   httpServer.handleClient();
 
-  if (Serial.available() > 0) {
+  while (Serial.available() > 0) {
     String cmnd = Serial.readStringUntil('\n');
+    cmnd.trim();
 
+    // wifi [<ssid> <pass>]
     if (cmnd.startsWith("wifi")) {
       if (cmnd.indexOf(' ') != -1) {  // STA
         cmnd = cmnd.substring(cmnd.indexOf(' ') + 1);
         String ssid = cmnd.substring(0, cmnd.indexOf(' '));
         cmnd = cmnd.substring(cmnd.indexOf(' ') + 1);
-        String pass = cmnd.substring(0, cmnd.indexOf(' '));
+        String pass = cmnd;
 
         WiFi.begin(ssid, pass);
         Serial.print("Connecting");
@@ -98,9 +169,30 @@ void loop() {
       }
     }
 
-    if (cmnd == "clear") {
-      gfx->fillScreen(BACKGROUND);
-      Serial.println("clear");
+    // clear [<color:ffffff>]
+    if (cmnd.startsWith("clear")) {
+      if (cmnd.indexOf(' ') != -1) {
+        cmnd = cmnd.substring(cmnd.indexOf(' ') + 1);
+        String color = cmnd.substring(0, cmnd.indexOf(' '));
+
+        int r = strtol(color.substring(0, 2).c_str(), 0, 16);
+        int g = strtol(color.substring(2, 4).c_str(), 0, 16);
+        int b = strtol(color.substring(3, 6).c_str(), 0, 16);
+
+        gfx->fillScreen(RGB565(r, g, b));
+        Serial.printf("clear %s\n", color);
+      } else {
+        gfx->fillScreen(BACKGROUND);
+        Serial.println("clear");
+      }
+    }
+
+    // backlight <value:0-256>
+    if (cmnd.startsWith("backlight")) {
+      cmnd = cmnd.substring(cmnd.indexOf(' ') + 1);
+      int value = cmnd.substring(0, cmnd.indexOf(' ')).toInt();
+      analogWrite(TFT_BL, max(0, min(256, value)));
+      Serial.printf("backlight %d\n", value);
     }
 
     // text <x> <y> <scale> <color:ffffff> <text>
@@ -115,7 +207,7 @@ void loop() {
       String color = cmnd.substring(0, cmnd.indexOf(' '));
       cmnd = cmnd.substring(cmnd.indexOf(' ') + 1);
       String text = cmnd;
-      Serial.printf("text:%s x:%d y:%d scale:%d color:%d\n", text, x, y, scale, color);
+      Serial.printf("text:%s x:%d y:%d scale:%d color:%s\n", text, x, y, scale, color);
 
       int r = strtol(color.substring(0, 2).c_str(), 0, 16);
       int g = strtol(color.substring(2, 4).c_str(), 0, 16);
@@ -127,8 +219,73 @@ void loop() {
       gfx->setUTF8Print(true);
       //gfx->setFont(u8g2_font_unifont_t_cyrillic);
       gfx->setFont(custom_cyrillic);
+      //gfx->setFont(custom_7_segm);
       gfx->println(text);
     }
+
+    // segmtext <x> <y> <scale> <color:ffffff> <segmtext>
+    if (cmnd.startsWith("segmtext")) {
+      cmnd = cmnd.substring(cmnd.indexOf(' ') + 1);
+      int x = cmnd.substring(0, cmnd.indexOf(' ')).toInt();
+      cmnd = cmnd.substring(cmnd.indexOf(' ') + 1);
+      int y = cmnd.substring(0, cmnd.indexOf(' ')).toInt();
+      cmnd = cmnd.substring(cmnd.indexOf(' ') + 1);
+      int scale = cmnd.substring(0, cmnd.indexOf(' ')).toInt();
+      cmnd = cmnd.substring(cmnd.indexOf(' ') + 1);
+      String color = cmnd.substring(0, cmnd.indexOf(' '));
+      cmnd = cmnd.substring(cmnd.indexOf(' ') + 1);
+      String segmtext = cmnd;
+      Serial.printf("segmtext:%s x:%d y:%d scale:%d color:%s\n", segmtext, x, y, scale, color);
+
+      int r = strtol(color.substring(0, 2).c_str(), 0, 16);
+      int g = strtol(color.substring(2, 4).c_str(), 0, 16);
+      int b = strtol(color.substring(3, 6).c_str(), 0, 16);
+
+      gfx->setCursor(x, y);
+      gfx->setTextSize(scale);
+      gfx->setTextColor(RGB565(r, g, b));
+      gfx->setUTF8Print(true);
+      //gfx->setFont(u8g2_font_unifont_t_cyrillic);
+      //gfx->setFont(custom_cyrillic);
+      gfx->setFont(custom_7_segm);
+      gfx->println(segmtext);
+    }
+
+    // cyrtext <x> <y> <scale> <color:ffffff> <text>
+    if (cmnd.startsWith("cyrtext")) {
+      cmnd = cmnd.substring(cmnd.indexOf(' ') + 1);
+      int x = cmnd.substring(0, cmnd.indexOf(' ')).toInt();
+      cmnd = cmnd.substring(cmnd.indexOf(' ') + 1);
+      int y = cmnd.substring(0, cmnd.indexOf(' ')).toInt();
+      cmnd = cmnd.substring(cmnd.indexOf(' ') + 1);
+      int scale = cmnd.substring(0, cmnd.indexOf(' ')).toInt();
+      cmnd = cmnd.substring(cmnd.indexOf(' ') + 1);
+      String color = cmnd.substring(0, cmnd.indexOf(' '));
+      cmnd = cmnd.substring(cmnd.indexOf(' ') + 1);
+      String text = cmnd;
+      Serial.printf("cyrtext:%s x:%d y:%d scale:%d color:%s\n", text, x, y, scale, color);
+
+      int r = strtol(color.substring(0, 2).c_str(), 0, 16);
+      int g = strtol(color.substring(2, 4).c_str(), 0, 16);
+      int b = strtol(color.substring(3, 6).c_str(), 0, 16);
+
+      gfx->setCursor(x, y);
+      gfx->setTextSize(scale);
+      gfx->setTextColor(RGB565(r, g, b));
+      gfx->setUTF8Print(true);
+      //gfx->setFont(u8g2_font_unifont_t_cyrillic);
+      gfx->setFont(custom_cyrillic);
+      //gfx->setFont(custom_7_segm);
+      String cyrText;
+      for (int i = 0; i < text.length(); i++) {  // convert lat characters to cyr
+        if (lat_cyr.find(text[i]) != lat_cyr.end())
+          cyrText += lat_cyr[text[i]];
+        else
+          cyrText += text[i];
+      }
+      gfx->println(cyrText);
+    }
+
 
     // image <x> <y> <width> <height> <img.bin>
     if (cmnd.startsWith("image")) {
@@ -142,16 +299,15 @@ void loop() {
       int height = cmnd.substring(0, cmnd.indexOf(' ')).toInt();
       cmnd = cmnd.substring(cmnd.indexOf(' ') + 1);
       String filename = cmnd;
-      Serial.printf("image:%s x:%d y:%d width:%d height:%d\n", filename, x, y, width, height);
+      Serial.printf("image:%s x:%d y:%d width:%d height:%d\n", filename.c_str(), x, y, width, height);
 
       File file = FFat.open("/" + filename);
       if (file.size() == 0) {
         Serial.println("Invalid file name");
       } else {
-        uint16_t *image = new uint16_t[width * height];
         file.read((uint8_t *)image, width * height * 2);
 
-        gfx->draw16bitRGBBitmap(x, y, image, width, height);
+        gfx->draw16bitRGBBitmap(x, y, (uint16_t *)image, width, height);
       }
       file.close();
     }
--- esp32-display/font/readme.txt
+++ esp32-display/font/readme.txt
@@ -1,5 +1,7 @@
 1. Get .ttf font
 2. otf2bdf -r 72 -p 60 <font.ttf> -o <font.bdf>
+ * http://www.komeil.com/blog/chortkeh-bdf-font-viewer-2-0
+ *copy 2x .dll
  - -r <DPI>
  - -p <point_size>
 3. bdfconv.exe -f 1 -m "0-127,1024-1279" -n custom_cyrillic -o <font.c> <font.bdf>
